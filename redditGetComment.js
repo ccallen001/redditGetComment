34705c81cf0aa57251eb6de49ff18a3f6c28eb18
Chase Allen <callen@smarterhq.com>
2017-03-04 18:06:30 -0500

    first commit

---
+++ redditGetComment.js
@@ -0,0 +1,63 @@
+//get DOM elements
+const username = document.querySelector(`input[data-ng-model="username"]`);
+
+//invoke angular
+angular.module(`app`, []).controller(`controller`, ($scope, $timeout) => {
+    const s = $scope;
+
+    let xhr,
+        resp;
+
+    //error handling
+    s.tryAnother = () => {
+        $timeout(() => {
+            s.comment = `Please try another one.`;
+            $timeout(() => {
+                s.username = null;
+                $timeout(() => { username.focus(); }, 300);
+            }, 333);
+        }, 2000);
+    }
+
+    //http request to retrieve data
+    s.getComment = ev => {
+        if (!s.username) {
+            s.comment = `Plese input a username.`;
+            $timeout(() => { username.focus(); }, 0);
+            console.log(s.username);
+        } else if ((/[^a-zA-Z0-9_-]/gm).test(s.username)) {
+            s.comment = `Hmm... That doesn't look like a valid username.`;
+            s.tryAnother();
+        } else {
+            try {
+                xhr = new XMLHttpRequest();
+                xhr.open(`get`, `https://www.reddit.com/user/${s.username}.json`, false);
+                xhr.send();
+                resp = JSON.parse(xhr.responseText);
+
+                if (!resp.data) {
+                    s.comment = `Oops! That user doesn't seem to exist..`;
+                    s.tryAnother();
+                } else {
+                    s.comment = resp.data.children[0].data.body;
+                }
+            } catch (err) {
+                console.log(err);
+                s.comment = `Whoops! There was an error. :( Please try again.`;
+                s.tryAnother();
+            }
+        }
+
+        if (s.comment == "" || s.comment == null || s.comment == undefined) {
+            s.comment = `Whoops! There was an error. :( Please try again.`;
+            s.tryAnother();
+        }
+    }
+
+    //handles key press on input
+    s.checkKey = ev => {
+        if (ev.keyCode === 13) {
+            s.getComment();
+        }
+    }
+});
